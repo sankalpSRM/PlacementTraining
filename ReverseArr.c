067538af4f7b746f0da9864082e50472a2646c23
sankalpSRM <113873509+sankalpSRM@users.noreply.github.com>
2025-01-21 11:57:46 +0530

    Update ReverseArr.c

--- ReverseArr.c
+++ ReverseArr.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main() {
-    int arr[] = {4,6,7,7,8987,5,6,89,8643};
-    //finding len of array
-    int size = sizeof(arr)/sizeof(arr[0]);
+    int size;
+    scanf("%d", &size);
+    int arr[size];
+    for(int i = 0;i<size;i++) {
+        scanf("%d", &arr[i]);
+    }
     //reversing
     for ( int i = 0, j = size -1; i<j; i++, j--)
         arr[i] = arr[i]+arr[j] - (arr[j]=arr[i]);
